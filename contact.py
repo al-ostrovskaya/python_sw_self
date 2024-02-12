009d3a3439aea66aea62dbc9e4ba29fdc24c5d89
schiwa <schiwa@schiwa.ru>
2024-02-12 23:51:27 +0300

    Задание 1,2,3.

---
+++ contact.py
@@ -0,0 +1,20 @@
+class Contact:
+    def __init__(self, firstname, middlename, lastname, nickname, title, company, address, home, mobile, work,
+                       fax, email1, email2, email3, year, year2):
+        self.firstname = firstname
+        self.lastname = lastname
+        self.middlename = middlename
+        self.nickname = nickname
+        self.title = title
+        self.company = company
+        self.address = address
+        self.home = home
+        self.mobile = mobile
+        self.work = work
+        self.fax = fax
+        self.email1 = email1
+        self.email2 = email2
+        self.email3 = email3
+        self.year = year
+        self.year2 = year2
+
--- test_add_group.py
+++ test_add_group.py
@@ -1,45 +1,72 @@
-from selenium import webdriver
+from selenium.webdriver.firefox.webdriver import WebDriver
 import unittest
+from group import Group
 
-class UntitledTestCase(unittest.TestCase):
+def is_element_present(self, how, what):
+    try:
+        self.wd.find_element(by=how, value=what)
+    except NoSuchElementException as e:
+        return False
+    return True
+
+
+class TestAddGroup(unittest.TestCase):
     def setUp(self):
-        self.wd = webdriver.Firefox()
+        self.wd = WebDriver()
         self.wd.implicitly_wait(30)
 
     def test_add_group(self):
         wd = self.wd
+        self.open_homepage(wd)
+        self.login(wd, "admin", "secret")
+        self.open_group_page(wd)
+        self.create_group(wd, Group("group", "logo", "comment"))
+        self.logout(wd)
+
+    def test_add_emptygroup(self):
+        wd = self.wd
+        self.open_homepage(wd)
+        self.login(wd, "admin", "secret")
+        self.open_group_page(wd)
+        self.create_group(wd, Group("", "", ""))
+        self.logout(wd)
+
+    def open_homepage(self, wd):
         wd.get("http://localhost/addressbook/")
+
+    def login(self, wd, username, password):
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys("admin")
+        wd.find_element_by_name("user").send_keys(username)
         wd.find_element_by_id("LoginForm").click()
         wd.find_element_by_name("pass").click()
         wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys("secret")
+        wd.find_element_by_name("pass").send_keys(password)
         wd.find_element_by_xpath("//input[@value='Login']").click()
+
+    def open_group_page(self, wd):
         wd.find_element_by_link_text("groups").click()
+
+    def create_group(self, wd, group):
         wd.find_element_by_name("new").click()
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys("test")
+        wd.find_element_by_name("group_name").send_keys(group.name)
         wd.find_element_by_name("group_header").click()
         wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys("logo")
+        wd.find_element_by_name("group_header").send_keys(group.header)
         wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys("comment")
+        wd.find_element_by_name("group_footer").send_keys(group.footer)
         wd.find_element_by_name("submit").click()
         wd.find_element_by_link_text("group page").click()
+
+    def logout(self, wd):
         wd.find_element_by_link_text("Logout").click()
-
-    def is_element_present(self, how, what):
-        try: self.wd.find_element(by=how, value=what)
-        except NoSuchElementException as e: return False
-        return True
-
 
     def tearDown(self):
         self.wd.quit()
+
 
 if __name__ == "__main__":
     unittest.main()
