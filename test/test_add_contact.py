9dc6813ae55c71fbc15ed1b7163dbf16b5203d42
schiwa13 <42767613+schiwa13@users.noreply.github.com>
2024-02-13 23:00:51 +0300

    Добавление теста удаления группы Создание общей фикстуры

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,12 +1,5 @@
-import pytest
-from fixture.application import Application
 from model.contact import Contact
 
-@pytest.fixture
-def app(request):
-    fixture = Application()
-    request.addfinalizer(fixture.destroy)
-    return fixture
 def test_add_contact(app):
     app.session.login("admin", "secret")
     app.contact.create(Contact("first", "middle", "last", "nickname", "title", "company", "address", "home", "mobile",
--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,11 +1,5 @@
-import pytest
-from fixture.application import Application
 from model.group import Group
-@pytest.fixture
-def app(request):
-    fixture = Application()
-    request.addfinalizer(fixture.destroy)
-    return fixture
+
 def test_add_group(app):
     app.session.login("admin", "secret")
     app.group.create(Group("group", "logo", "comment"))
@@ -14,4 +8,9 @@ def test_add_group(app):
 def test_add_emptygroup(app):
     app.session.login("admin", "secret")
     app.group.create(Group("", "", ""))
+    app.session.logout()
+
+def test_del_group(app):
+    app.session.login("admin", "secret")
+    app.group.delete_first_group()
     app.session.logout()
