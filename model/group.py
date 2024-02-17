d83ea85ff7650aa084e73938e3b8ecc8312d4a64
schiwa13 <42767613+schiwa13@users.noreply.github.com>
2024-02-17 18:42:55 +0300

    Изменен код модификации значений полей групп

--- model/group.py
+++ model/group.py
@@ -1,8 +1,6 @@
 class Group:
-    def __init__(self, name, header,footer,modify_name, modify_header, modify_footer):
+    def __init__(self, name=None, header=None, footer=None):
         self.name = name
         self.header = header
         self.footer = footer
-        self.modify_name = modify_name
-        self.modify_header = modify_header
-        self.modify_footer = modify_footer
+
--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -1,5 +1,9 @@
 from model.group import Group
-def test_modify_group(app):
+def test_modify_name(app):
     app.session.login("admin", "secret")
-    app.group.modify(Group("", "", "", "modify_name", "modify_header", "modify_footer"))
+    app.group.test_modify_name(Group(name="New_name"))
+    app.session.logout()
+def test_modify_header(app):
+    app.session.login("admin", "secret")
+    app.group.test_modify_name(Group(header="New_header"))
     app.session.logout()
